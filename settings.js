7d4453d28727e01bda1befa559839cea3c44f26a
Allpin23 <106477620+Allpin23@users.noreply.github.com>
2022-06-21 23:05:14 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,23 +18,23 @@
   global.available = false // Ubah Ke false Jika Bot Mu Tidak Ingin Terlihat Online
   global.autoTyping = false // Ubah Ke true Jika Ingin Saat Ada Yang Chat Bot Auto Mengetik
   global.autoRecord = true // Ubah Ke false Jika Tidak Ingin Saat Ada Yang Chat Bot Auto Merekam
-  global.multiplier = 69 // Semakin Besar Angkanya Semakin Sulit Untuk Naik Level
-  global.limitCount = 70 // 70 Dah Pas Banh
+  global.multiplier = 89 // Semakin Besar Angkanya Semakin Sulit Untuk Naik Level
+  global.limitCount = 10 // 70 Dah Pas Banh
   global.prefa = ['','.','/','#']
 
   // Other
-  global.ownername = 'Riy' // Ganti Jadi Namamu
-  global.owner = ['6281575886399','6281575886399','6285813334446'] // Kalo Mau Lebih Tambahin Aja
-  global.ownernomer = '6281575886399' // Owner Utama Istilahnya
-  global.botname = 'Hinata Hyuga' // Ganti Jadi Nama Botmu
-  global.packname = 'Hinata Hyuga' // Sticker Weem
-  global.author = 'Riy' // Sticker Weem
-  global.webme = 'https://api-riy.herokuapp.com' // Ganti Sama Web Mu
-  global.linkgroup = 'https://chat.whatsapp.com/HZhLynp6E9QKNrRFtUp26T' // Ganti Sama Link Gc Mu
+  global.ownername = 'PemilikBot:089648311511' // Ganti Jadi Namamu
+  global.owner = ['6289648311511','6289648311511','6289648311511'] // Kalo Mau Lebih Tambahin Aja
+  global.ownernomer = '6289648311511' // Owner Utama Istilahnya
+  global.botname = 'AlphinBotz' // Ganti Jadi Nama Botmu
+  global.packname = 'AlphinBotz' // Sticker Weem
+  global.author = '089648311511' // Sticker Weem
+  global.webme = 'https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh' // Ganti Sama Web Mu
+  global.linkgroup = 'https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh' // Ganti Sama Link Gc Mu
   global.sessionName = 'session'
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw' // Ganti Sama Link Yt Mu
-  global.github = 'https://github.com/riychdwayne' // Ganti Sama Link Github Mu
-  global.instagram = 'https://instagram.com/riychdwayne' // Ganti Sama Link Ig mu
+  global.youtube = 'https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh' // Ganti Sama Link Yt Mu
+  global.github = 'https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh' // Ganti Sama Link Github Mu
+  global.instagram = 'https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh' // Ganti Sama Link Ig mu
 
   // Background Setiap Menu
   global.bg1 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -43,11 +43,11 @@
   global.bg4 = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fun-run-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
   // Tips Menu
-  global.tips1 = 'Gunakan Jeda 3 Detik Agar Bot Tidak Mati Akibat Spam 🚀'
-  global.tips2 = 'Jika Kamu Ingin Bermain RPG Hubungi Admin Untuk Mengaktifkan Fitur RPG ⚔️'
-  global.tips3 = 'Jika Kamu Ingin Request/Repport Fitur, Cukup Ketik *.repport Laporannya* 👮'
-  global.tips4 = 'Jika Kamu Sedang Gabut, Kamu Bisa Memainkan Fitur Game Yang Tersedia 🕹️'
-  global.tips5 = 'Jika Kamu Ingin Mendownload Video Youtube, Cukup Ketik *.ytmp4 <link>* 📥'
+  global.tips1 = 'Nomor pemilik bot Wa.me/6289648311511 Grup resmi bot: https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh'
+  global.tips2 = 'Nomor pemilik bot Wa.me/6289648311511 Grup resmi bot: https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh'
+  global.tips3 = 'Nomor pemilik bot Wa.me/6289648311511 Grup resmi bot: https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh'
+  global.tips4 = 'Nomor pemilik bot Wa.me/6289648311511 Grup resmi bot: https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh'
+  global.tips5 = 'Nomor pemilik bot Wa.me/6289648311511 Grup resmi bot: https://chat.whatsapp.com/Dse5mkgbOetEX7a0Rvofjh'
 
   // Edit Sesuai Kemauan Mu
   global.rpg = {
